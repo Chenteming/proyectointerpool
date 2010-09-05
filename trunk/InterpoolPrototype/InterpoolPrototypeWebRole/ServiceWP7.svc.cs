09ce9df86f4d7398edc53416934adaee609e5e70
fedetrini@gmail.com <fedetrini@gmail.com@ef7d9822-55ab-29b5-15d1-295a5a4f5466>
2010-09-05 19:50:29 +0000

    va lo de bing

--- trunk/InterpoolPrototype/InterpoolPrototypeWebRole/ServiceWP7.svc.cs
+++ trunk/InterpoolPrototype/InterpoolPrototypeWebRole/ServiceWP7.svc.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using InterpoolPrototypeWebRole.ServicioBusquedaBing;
 
 namespace InterpoolPrototypeWebRole
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServiceWP7" in code, svc and config file together.
     public class ServiceWP7 : IServiceWP7
     {
+        private static int counterCities;
+
+        private static string currentCity;
+
         public List<string> GetCities()
         {
             List<string> res = new List<string>();
@@ -23,17 +28,130 @@ namespace InterpoolPrototypeWebRole
         }
 
         public List<string> GetProbablySuspects()
+        public string GetClueByFamous(string cadena)
         {
-            // TODO: Get from the database the lists of names of the friends
-            // of the current user
-            List<string> friendsNames = new List<string>();
-            HelloWorldEntities context = new HelloWorldEntities();
-            List<PrototypeSuspect> pSuspects = new List<PrototypeSuspect>(context.PrototypeSuspects);
-            foreach (PrototypeSuspect ps in pSuspects)
+            string resultado = "";
+
+            ServicioBusquedaBing.LiveSearchPortTypeClient client = new InterpoolPrototypeWebRole.ServicioBusquedaBing.LiveSearchPortTypeClient();
+
+            SearchRequest request = new SearchRequest()
+
             {
-                friendsNames.Add(ps.Name);
-            }
-            return friendsNames;
+
+                AppId = "A00C4105122186E4F9F0DFD82CDF594DD866BC1F",
+
+                Sources = new SourceType[] { SourceType.Web, SourceType.News },
+
+                Adult = AdultOption.Moderate,
+
+                AdultSpecified = true,
+
+                Query = cadena,      
+         
+                Market = "es-MX",
+            };
+
+            SearchResponse response = client.Search(request);
+
+            if (response.Errors == null)
+                resultado = response.News.Results.FirstOrDefault().Snippet;
+
+            return resultado;
+
         }
+
+        //Start game.
+        public void StartGame()
+        {
+            counterCities = 0;
+            currentCity = "Montevideo";
+        }
+
+        //Returns the current city
+        public string GetCurrentCity()
+        {
+            return currentCity;
+        }
+
+        //Returns possible cities to travel
+        public List<string> GetPossibleCities()
+        {
+            List<string> listCities = new List<string>();
+            switch (counterCities)
+            {
+                case 0:
+                    listCities.Add("Rio de Janeiro");
+                    listCities.Add("Katmandu");
+                    listCities.Add("Katmandu");
+                    break;
+                case 1:
+                    listCities.Add("Roma");
+                    listCities.Add("Madrid");
+                    listCities.Add("Madrid");
+                    break;
+                case 2:
+                    listCities.Add("Londres");
+                    listCities.Add("Moscu");
+                    listCities.Add("Moscu");
+                    break;
+            }
+            listCities.Add("NoPuedeSeguirViajando");
+            listCities.Add("NoPuedeSeguirViajando");
+            listCities.Add("NoPuedeSeguirViajando");
+            return listCities;
+        }
+
+        //Returns the current city famous
+        public List<string> GetCurrentFamous(string city)
+        {
+            List<string> listFamous = new List<string>();
+            switch (city)
+            {
+                case "Montevideo":
+                    listFamous.Add("Sebastian Abreu");
+                    listFamous.Add("Natalia Oreiro");
+                    listFamous.Add("Paco Casal");
+                    break;
+                case "Río de Janeiro":
+                    listFamous.Add("Roberto Carlos");
+                    listFamous.Add("Xuxa");
+                    listFamous.Add("Ronaldo");
+                    break;
+                case "Roma":
+                    listFamous.Add("Giorgio Armani");
+                    listFamous.Add("Andrea Bocelli");
+                    listFamous.Add("Elisabetta Gregoracci");
+                    break;
+            }
+            listFamous.Add("Queen Elizabeth");
+            listFamous.Add("Bruce Dickinson");
+            listFamous.Add("Steven Gerrard");
+            return listFamous;
+        }
+
+        //Esta es la funcion de facebook que tiene Fede A y Vicente.
+        public string GetProbablySuspects()
+        {
+            return "GetProbablySuspects";
+        }
+
+       //Travel from one city to another
+        public void Travel(string City)
+        {
+            counterCities++;
+            switch (counterCities)
+            {
+                case 1:
+                    currentCity = "Río de Janeiro";
+                    break;
+                case 2:
+                    currentCity = "Roma";
+                    break;
+                case 3:
+                    currentCity = "Londres";
+                    break;
+            }
+        }
+
     }
 }
