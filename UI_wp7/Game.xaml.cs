9eac0ead52e1a8b3048a73eaa2abd2ba60778e1f
vicenteacosta3@gmail.com <vicenteacosta3@gmail.com@ef7d9822-55ab-29b5-15d1-295a5a4f5466>
2010-09-12 17:46:00 +0000

    Subo al repo un tag con el cliente del WP7.

---
+++ UI_wp7/Game.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace UIPrototype
+{
+    public partial class Game : PhoneApplicationPage
+    {
+        public Game()
+        {
+            InitializeComponent();
+        }
+
+        private void ComputerButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Famous.xaml", UriKind.RelativeOrAbsolute));
+        }
+		
+		private void DoorButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+		
+		private void FilesButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Suspect.xaml", UriKind.RelativeOrAbsolute));
+        }
+		
+		private void MapButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/City.xaml", UriKind.RelativeOrAbsolute));
+        }
+	}
+}
---
+++ UI_wp7/GameManager.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using Microsoft.Phone.Controls;
+
+namespace UIPrototype
+{
+    public class GameManager
+    {
+
+        private static GameManager instance;
+        private String currentCity;
+        private List<String> cities;
+        private List<String> clues;
+        private List<String> famous;
+        private List<String> suspects;
+
+        public static GameManager getInstance()
+        {
+            if (instance == null)
+                instance = new GameManager();
+            return instance;
+        }
+
+        private GameManager()
+        {
+            cities = new List<String>();
+            famous = new List<String>();
+            clues = new List<String>();
+            suspects = new List<String>();
+        }
+
+        public void AddCity(int position, String name)
+        // Add the new city in the list
+        {
+            cities.Insert(position, name);
+        }
+
+        public void AddClue(int position, String name)
+        // Add the new clue in the list
+        {
+            clues.Insert(position, name);
+        }
+
+        public void AddFamous(int position, String name)
+        // Add the new famous the list
+        {
+            famous.Insert(position, name);
+        }
+
+        public void SetCurrentCity(String city)
+        {
+            currentCity = city;
+        }
+
+        public void SetCurrentCities(List<String> list)
+        {
+            cities = list;
+        }
+
+        public void SetCurrentFamous(List<String> list)
+        {
+            famous = list;
+        }
+
+        public void RemoveCities()
+        // Remove all the elements of the list
+        {
+            cities.RemoveRange(0, 2);
+        }
+
+        public void RemoveClues()
+        // Remove all the elements of the list
+        {
+            clues.RemoveRange(0, 2);
+        }
+
+        public void RemoveFamous()
+        // Remove all the elements of the list
+        {
+            famous.RemoveRange(0, 2);
+        }
+
+        public List<String> GetCities()
+        // Return all the cities	
+        {
+            return cities;
+        }
+
+        public List<String> GetClues()
+        // Return all the clues	
+        {
+            return clues;
+        }
+
+        public List<String> GetFamous()
+        // Return all the famous	
+        {
+            return famous;
+        }
+
+        public String GetCurrentCity()
+        // Return the current city
+        {
+            return currentCity;
+        }
+
+        public List<String> GetSuspects()
+        {
+            return suspects;
+        }
+
+        public void SetSuspectsList(List<String> list)
+        {
+            suspects = list;
+        }
+
+    }
+}
