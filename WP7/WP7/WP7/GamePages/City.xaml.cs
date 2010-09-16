6b46180ab66de30b72036520bb6258389ffc5260
vicenteacosta3@gmail.com <vicenteacosta3@gmail.com@ef7d9822-55ab-29b5-15d1-295a5a4f5466>
2010-09-16 22:18:42 +0000

    Subo el proyecto para el Windows Phone 7

---
+++ WP7/WP7/WP7/GamePages/City.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using WP7.ServiceReference;
+
+
+namespace WP7
+{
+    public partial class City : PhoneApplicationPage
+    {
+        private ServiceWP7Client client;
+
+        public City()
+        {
+            InitializeComponent();
+            client = new ServiceWP7Client();
+            client.GetPossibleCitiesCompleted += new EventHandler<GetPossibleCitiesCompletedEventArgs>(GetPossibleCitiesCompleted);
+            client.GetPossibleCitiesAsync();
+            client.CloseCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(client_CloseCompleted);
+            client.CloseAsync();
+            Travel1.Visibility = System.Windows.Visibility.Collapsed;
+            Travel2.Visibility = System.Windows.Visibility.Collapsed;
+            Travel3.Visibility = System.Windows.Visibility.Collapsed;
+        }
+
+        private void Travel1_Click(object sender, RoutedEventArgs e)
+        {
+            ServiceWP7Client client = new ServiceWP7Client();
+            client.TravelCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(client_TravelCompleted);
+            client.TravelAsync(Travel1.Content.ToString());
+            client.CloseCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(client_CloseCompleted);
+            client.CloseAsync();
+            GameManager gm = GameManager.getInstance();
+            gm.SetCurrentCity(Travel1.Content.ToString());
+        }
+
+        private void Travel2_Click(object sender, RoutedEventArgs e)
+        {
+            ServiceWP7Client client = new ServiceWP7Client();
+            client.TravelCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(client_TravelCompleted);
+            client.TravelAsync(Travel2.Content.ToString());
+            client.CloseCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(client_CloseCompleted);
+            client.CloseAsync();
+            GameManager gm = GameManager.getInstance();
+            gm.SetCurrentCity(Travel2.Content.ToString());
+        }
+
+        private void Travel3_Click(object sender, RoutedEventArgs e)
+        {
+            ServiceWP7Client client = new ServiceWP7Client();
+            client.TravelCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(client_TravelCompleted);
+            client.TravelAsync(Travel3.Content.ToString());
+            client.CloseCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(client_CloseCompleted);
+            client.CloseAsync();
+            GameManager gm = GameManager.getInstance();
+            gm.SetCurrentCity(Travel3.Content.ToString());
+
+        }
+
+        // Asynchronous callbacks for displaying results.
+
+        public void GetPossibleCitiesCompleted(object sender, GetPossibleCitiesCompletedEventArgs e)
+        {
+            List<String> cities = e.Result.ToList();
+            GameManager gm = GameManager.getInstance();
+            gm.SetCurrentCities(cities);
+
+            Travel1.Visibility = System.Windows.Visibility.Visible;
+            Travel2.Visibility = System.Windows.Visibility.Visible;
+            Travel3.Visibility = System.Windows.Visibility.Visible;
+
+            Travel1.Content = cities.ElementAt(0);
+            Travel2.Content = cities.ElementAt(1);
+            Travel3.Content = cities.ElementAt(2);
+        }
+
+        void client_CloseCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            //TODO
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/GamePages/Game.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        void client_TravelCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WP7/WP7/WP7/GamePages/Suspect.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using WP7.ServiceReference;
+
+namespace WP7
+{
+    public partial class Suspect : PhoneApplicationPage
+    {
+        private List<String> suspectsList;
+        private static int index;
+        public Suspect()
+        {
+            InitializeComponent();
+            ServiceWP7Client client = new ServiceWP7Client();
+            client.GetProbablySuspectsCompleted += new EventHandler<GetProbablySuspectsCompletedEventArgs>(GetProbablySuspectsCallback);
+            client.GetProbablySuspectsAsync();
+            client.CloseAsync();
+            suspectsList = new List<String>();
+        }
+
+        public void GetProbablySuspectsCallback(object sender, GetProbablySuspectsCompletedEventArgs e)
+        {
+            GameManager gm = GameManager.getInstance();
+            List<String> list = e.Result.ToList();
+            gm.SetSuspectsList(list);
+            index = 0;
+            if (list.Count == 0)
+                Name_Suspect.Text = "There are no suspects";
+            else
+                Name_Suspect.Text = list.ElementAt(0);
+        }
+
+        private void LeftArrow_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            GameManager gm = GameManager.getInstance();
+            List<String> suspectsList = gm.GetSuspects();
+            if (suspectsList.Count == 0)
+                Name_Suspect.Text = "There are no suspects";
+            else
+                if (index == 0)
+                    Name_Suspect.Text = suspectsList.ElementAt(0);
+                else
+                {
+                    index--;
+                    Name_Suspect.Text = suspectsList.ElementAt(index);
+                }
+        }
+
+        private void RightArrow_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            GameManager gm = GameManager.getInstance();
+            List<String> suspectsList = gm.GetSuspects();
+            if (suspectsList.Count == 0)
+                Name_Suspect.Text = "There are no suspects";
+            else
+                if (index == suspectsList.Count - 1)
+                    Name_Suspect.Text = suspectsList.ElementAt(suspectsList.Count - 1);
+                else
+                {
+                    index++;
+                    Name_Suspect.Text = suspectsList.ElementAt(index);
+                }
+        }
+    }
+}
