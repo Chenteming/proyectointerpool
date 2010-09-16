883b39b7f407e5afdb57bb9fcf445412f46fa5c9
vicenteacosta3@gmail.com <vicenteacosta3@gmail.com@ef7d9822-55ab-29b5-15d1-295a5a4f5466>
2010-09-16 22:18:42 +0000

    Subo el proyecto para el Windows Phone 7

---
+++ trunk/WP7/WP7/WP7/GamePages/Famous.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using WP7.ServiceReference;
+
+
+namespace WP7
+{
+    public partial class Famous : PhoneApplicationPage
+    {
+        private ServiceWP7Client client;
+
+        public Famous()
+        {
+            InitializeComponent();
+            GameManager gm = GameManager.getInstance();    
+        
+            client = new ServiceWP7Client();
+            client.GetCurrentFamousCompleted += new EventHandler<GetCurrentFamousCompletedEventArgs>(client_GetCurrentFamousCompleted);
+            client.GetCurrentFamousAsync(gm.GetCurrentCity());
+
+            client.CloseCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(client_CloseCompleted);
+            client.CloseAsync();
+            // Set the textboxes with the name of the famous
+            Famous1.Visibility = System.Windows.Visibility.Collapsed;
+            Famous2.Visibility = System.Windows.Visibility.Collapsed;
+            Famous3.Visibility = System.Windows.Visibility.Collapsed;            
+        }       
+		
+		public void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+		{
+		}
+
+        private void Famous1_Click(object sender, RoutedEventArgs e)
+        {
+            client = new ServiceWP7Client();
+            client.GetClueByFamousCompleted += new EventHandler<GetClueByFamousCompletedEventArgs>(GetClueByFamousCallback);          
+            client.GetClueByFamousAsync(Famous1.Content.ToString());
+            client.CloseCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(client_CloseCompleted);
+            client.CloseAsync();
+        }
+
+        private void Famous2_Click(object sender, RoutedEventArgs e)
+        {
+            client = new ServiceWP7Client();
+            client.GetClueByFamousCompleted += new EventHandler<GetClueByFamousCompletedEventArgs>(GetClueByFamousCallback);
+            client.GetClueByFamousAsync(Famous2.Content.ToString());
+            client.CloseCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(client_CloseCompleted);
+            client.CloseAsync();
+        }
+
+        private void Famous3_Click(object sender, RoutedEventArgs e)
+        {            
+            client = new ServiceWP7Client();
+            client.GetClueByFamousCompleted += new EventHandler<GetClueByFamousCompletedEventArgs>(GetClueByFamousCallback);
+            client.GetClueByFamousAsync(Famous3.Content.ToString());            
+            client.CloseCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(client_CloseCompleted);
+            client.CloseAsync();
+        }
+
+        private void GetClueByFamousCallback(object sender, GetClueByFamousCompletedEventArgs e)
+        {
+            String clue = e.Result;
+            Clue.Text = clue;           
+        }
+        void client_GetCurrentFamousCompleted(object sender, GetCurrentFamousCompletedEventArgs e)
+        {
+            GameManager gm = GameManager.getInstance();
+            gm.SetCurrentFamous(e.Result.ToList());            
+            List<String> famous = gm.GetFamous();
+
+            Famous1.Visibility = System.Windows.Visibility.Visible;
+            Famous2.Visibility = System.Windows.Visibility.Visible;
+            Famous3.Visibility = System.Windows.Visibility.Visible;
+
+            //Show in the textBoxes the name of the famous
+            Famous1.Content = famous.ElementAt(0);
+            Famous2.Content = famous.ElementAt(1);
+            Famous3.Content = famous.ElementAt(2);
+
+            
+        }
+
+        void client_CloseCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            //TODO
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/GamePages/Game.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
---
+++ trunk/WP7/WP7/WP7/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using WP7.ServiceReference;
+
+namespace WP7
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private ServiceWP7Client client;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            client = new ServiceWP7Client();
+            client.StartGameCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(client_StartGameCompleted);
+            client.StartGameAsync();
+            client.CloseCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(client_CloseCompleted);
+            client.CloseAsync();
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            GameManager gm = GameManager.getInstance();
+            client = new ServiceWP7Client();
+            client.GetCurrentCityCompleted += new EventHandler<GetCurrentCityCompletedEventArgs>(GetCurrentCityCallback);
+            client.GetCurrentCityAsync();
+            client.CloseCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(client_CloseCompleted);
+            client.CloseAsync();
+            NavigationService.Navigate(new Uri("/GamePages/Game.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+		// Asynchronous callbacks for displaying results.
+        void client_CloseCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+
+        }
+
+        void client_StartGameCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+
+        }
+        
+        static void GetCurrentCityCallback(object sender, GetCurrentCityCompletedEventArgs e)
+        {
+            String initialCity = e.Result;
+            GameManager gm = GameManager.getInstance();
+            gm.SetCurrentCity(initialCity);
+        }
+    }
+}
