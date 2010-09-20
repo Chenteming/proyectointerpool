68059e1983617f2123a56a65a064b9abc9b2e0c0
diegoem6@gmail.com <diegoem6@gmail.com@ef7d9822-55ab-29b5-15d1-295a5a4f5466>
2010-09-20 23:17:18 +0000

    Controller e Interfaze

---
+++ InterpoolPrototype/InterpoolPrototypeWebRole/Controller/IProcessController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InterpoolPrototypeWebRole.Data;
+
+
+namespace InterpoolPrototypeWebRole.Controller
+{
+    interface IProcessController
+    {
+        public void StartGame(User user);
+        public void CreateClue();
+        public void GetSuspects();
+        public void BuiltTravel(User user);
+        public IQueryable<Clue> CreateClue(City city, User user, Suspect suspect);
+
+    }
+}
---
+++ InterpoolPrototype/InterpoolPrototypeWebRole/Controller/ProcessController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using InterpoolPrototypeWebRole.Data;
+
+namespace InterpoolPrototypeWebRole.Controller
+{
+    public class ProcessController:IProcessController
+    {
+        public void StartGame(User user)
+        {
+            // this is only the structs that we should follow
+
+            // 1 the trip is built to be followed by user
+            BuiltTravel(user);
+
+            // 2 Get suspects
+            GetSuspects();
+
+            // 3 Create clues
+            CreateClue();
+        }
+
+        public void CreateClue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetSuspects()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BuiltTravel(User user)
+        {
+            Game newGame = new Game();
+            user.Game = newGame;
+            IDataManager dm = new DataManager();
+
+            List<City> selectedCities = new List<City>();
+            NodePath node;
+            //TODO, maybe the amount of NodePath should be a param in the data base
+            for (int i = 0; i < 4; i++)
+            {
+                node = new NodePath();
+                bool find = false;
+                do
+                {
+                    //get one city random
+                    //if not city in selectedCities
+                    //  find = true
+                    //  node.City = city
+                    //  node.Famous.Add(new List<Famous> (dm.getFamousByCity(city)))
+                } while (!find);
+                newGame.NodePath.Add(node);
+            }
+        }
+
+        public IQueryable<Clue> CreateClue(City city, User user, Suspect suspect)
+        {
+            return null;
+            
+        }
+    }
+}
