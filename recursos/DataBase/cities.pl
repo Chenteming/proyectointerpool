cf103a1d237c3a5c7e773e94ef7483a55c7edc0e
mtincho12 <mtincho12@ef7d9822-55ab-29b5-15d1-295a5a4f5466>
2010-09-22 01:05:02 +0000

    Se le agrego un numero de ciudad para podes elegir de forma randomica

--- recursos/DataBase/cities.pl
+++ recursos/DataBase/cities.pl
@@ -1,5 +1,7 @@
+$number = 1;
 while ( <> ){
 	if(/\d+.-.(.*),.(.*)$/){
-		print "insert into [interpooldb].[dbo].[Cities] (CityName, CityCountry, Level_LevelId) values (\'" . $1 . "\',\'" .  $2 .  "\', (Select max(LevelId) FROM [interpooldb].[dbo].[Levels]));" . "\n";
+		print "insert into [interpooldb].[dbo].[Cities] (CityName, CityCountry, CityNumber, Level_LevelId) values (\'" . $1 . "\',\'" .  $2 .  "\', $number, (Select max(LevelId) FROM [interpooldb].[dbo].[Levels]));" . "\n";
+		$number = $number + 1;
 	}
 }
