f8427ccce825ca0bb0204275bd21ad0f2c265718
vicenteacosta3@gmail.com <vicenteacosta3@gmail.com@ef7d9822-55ab-29b5-15d1-295a5a4f5466>
2010-09-25 21:42:36 +0000

    Agrego constante de direccoin a la que se dirige luego del login en Facebook

--- trunk/InterpoolCloud/InterpoolCloudWebRole/Utilities/Constants.cs
+++ trunk/InterpoolCloud/InterpoolCloudWebRole/Utilities/Constants.cs
@@ -9,5 +9,7 @@ namespace InterpoolCloudWebRole.Utilities
     {
         public static int NUMBERLASTCITY = 4;
         public static int MAX_SUSPECTS = 10;
+        public static string REDIRECT_URL_AFTER_LOGIN_FACEBOOK = "http://127.0.0.1:81/Default.aspx/";
+        // public static string REDIRECT_URL_AFTER_LOGIN_FACEBOOK = "http://servicewp7.cloudapp.net/Default.aspx";
     }
 }
