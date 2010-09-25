9956bea8200ff6bd3e6067b0f31f532a3d3b196f
diegoem6@gmail.com <diegoem6@gmail.com@ef7d9822-55ab-29b5-15d1-295a5a4f5466>
2010-09-25 19:21:14 +0000

    createClue andando

--- trunk/InterpoolCloud/InterpoolCloudWebRole/Controller/ProcessController.cs
+++ trunk/InterpoolCloud/InterpoolCloudWebRole/Controller/ProcessController.cs
@@ -35,29 +35,48 @@ namespace InterpoolCloudWebRole.Controller
             InterpoolContainer conteiner = new InterpoolContainer();
             try
             {
+                user.Level = conteiner.Levels.Where(l => l.LevelId == 1).First();
                 // 1 the trip is built to be followed by user
                 Game newGame = BuiltTravel(user, conteiner);
-
+                
                 // 2 Get suspects
-                GetSuspects(newGame);
+                GetSuspects(newGame,conteiner);
 
                 // 3 Create clues
-                CreateClue(newGame);
+                CreateClue(newGame,conteiner);
                 
                 conteiner.AddToGames(newGame);
+                conteiner.SaveChanges();
             }
             catch (Exception e)
             {
+                Log log = new Log();
+                log.LogName = "StartGame";
+                log.LogStackTrace = e.StackTrace;
+                //conteiner.AddToLogs(log);
                 throw e;
             }
         }
 
-        public void GetSuspects(Game newGame)
+        public void GetSuspects(Game newGame, InterpoolContainer container)
         {
             // in this operation we should to find the possibles suspects , and asign the suspect
-            // newGame.Suspect =
-            // newGame.PossibleSuspect
-            throw new NotImplementedException();
+
+            IQueryable<Suspect> IQbigSuspect =  from s in container.Suspects
+                                                where s.SuspectId == 3
+                                                select s;
+            newGame.Suspect = IQbigSuspect.First();
+            IQueryable<Suspect> IQSuspects = from s in container.Suspects
+                                               where s.SuspectId != 3
+                                               select s;
+            
+            foreach (Suspect s in IQSuspects.ToList<Suspect>())
+            {
+                newGame.PossibleSuspect.Add(s);
+
+            }
+
+
         }
 
         private Game BuiltTravel(User user, InterpoolContainer conteiner)
@@ -90,14 +109,16 @@ namespace InterpoolCloudWebRole.Controller
                     {
                         find = true;
                         node.City = next;
-                        foreach (Famous f in dm.GetFamousByCity(next, conteiner))
+                        List<Famous> listFamous = next.Famous.ToList<Famous>();
+                        foreach (Famous f in listFamous)
                         {
                             node.Famous.Add(f);
                         } 
                     }
                 } while (!find);
+                node.NodePathOrder = i;
                 newGame.NodePath.Add(node);
-                conteiner.AddToNodePaths(node);
+                //conteiner.AddToNodePaths(node);
             }
             return newGame;
         }
@@ -107,20 +128,24 @@ namespace InterpoolCloudWebRole.Controller
          * - first clue is final
          * - second clue is dynamic
          * - third clue only have news of the famous*/
-        private void CreateClue(Game g)
+        private void CreateClue(Game g, InterpoolContainer conteiner)
         {
             DataManager dm = new DataManager();
 
             /* define the number of characteristics of the suspect by city */
-            List<Int32> amountCharacteristicsSuspects = new List<int>(3);
-            amountCharacteristicsSuspects[0]=1; amountCharacteristicsSuspects[1]=2; amountCharacteristicsSuspects[2]=3;
+            int[] amountCharacteristicsSuspects = new int[3];
+            amountCharacteristicsSuspects[0]=1; 
+            amountCharacteristicsSuspects[1]=2; 
+            amountCharacteristicsSuspects[2]=2;
 
             /* built the structure for the characteristics that I will put on each clue */
-            List<Boolean> cSuspect = new List<Boolean>(5);
-            cSuspect[0]=false;cSuspect[1]=false;cSuspect[2]=false;cSuspect[3]=false;cSuspect[4]=false;
+            bool[] cSuspect = new bool[5];
+            cSuspect[0] = true;
+            cSuspect[1] = true;
+            cSuspect[2] = true;
+            cSuspect[3] = true;
+            cSuspect[4] = true;
             
-            /* get the context for query of DataManager*/
-            InterpoolContainer ic = new InterpoolContainer();
 
             /* iterates over the NodePath of the game */
             int i;
@@ -132,13 +157,13 @@ namespace InterpoolCloudWebRole.Controller
                 Random r = new Random();
 
                 /* get the amount of caracteristic of the suspect by NodePath  */
-                int rnd = r.Next(0, 2);
-                while (amountCharacteristicsSuspects[rnd] != 0)
+                int rnd = r.Next(0, 3);
+                while (amountCharacteristicsSuspects[rnd] == -1)
                 {
-                    rnd = r.Next(0, 2);
+                    rnd = r.Next(0, 3);
                 }
                 int characteristicsSuspect = amountCharacteristicsSuspects[rnd];
-                amountCharacteristicsSuspects[rnd] = 0;
+                amountCharacteristicsSuspects[rnd] = -1;
                 /* get the next city by NodePath */
                 City nextCity = NextCity(g, cnp);
                 /* if nextCity is null, break the cicle */
@@ -151,9 +176,18 @@ namespace InterpoolCloudWebRole.Controller
                 /* set the city */
                 c3.City = cnp.City;
                 /* if i have to put characteristics on the clue of the suspect */
+                Famous famous = cnp.Famous.First();
                 if (characteristicsSuspect != 0)
                 {
-                    c3.ClueContent = GetRandomCharacteristicSuspect(g.Suspect, cSuspect) + cnp.Famous.First().New;
+
+                    if (famous.New.Count != 0 && famous.New.First()!=null)
+                    {
+                        c3.ClueContent = GetRandomCharacteristicSuspect(g.Suspect, cSuspect) + " " +famous.New.First().NewContent;
+                    }
+                    else
+                    {
+                        c3.ClueContent = GetRandomCharacteristicSuspect(g.Suspect, cSuspect);
+                    }
                     characteristicsSuspect--;
                 }
                 
@@ -161,19 +195,43 @@ namespace InterpoolCloudWebRole.Controller
                 Clue c2 = new Clue();
 
                 /* get the dynamic cityProperty for the nextCity */
-                CityProperty cpd = nextCity.CityProperty.Where(qcp => qcp.Dyn == true).First();
+                var queryCity = nextCity.CityProperty.Where(qcp => qcp.Dyn == true);
+                CityProperty cpd = null;
+                if (queryCity.Count() != 0 && queryCity.First()!=null)
+                {
+                    cpd = queryCity.First();
+                }
+                
 
                 /* set de city */
                 c2.City = cnp.City;
                 /* if i have to put characteristics on the clue of the suspect */
+                famous = cnp.Famous.ElementAt(1);
+                string dynProperty = cpd == null ? "" : cpd.CityPropertyContent;
                 if (characteristicsSuspect != 0)
                 {
-                    c2.ClueContent = cpd.CityPropertyContent + GetRandomCharacteristicSuspect(g.Suspect, cSuspect) + cnp.Famous.ElementAt(1).New;
+
+                    
+                    if (famous.New.Count() != 0 && famous.New.First() != null)
+                    {
+                        c2.ClueContent = dynProperty + " " + GetRandomCharacteristicSuspect(g.Suspect, cSuspect) + " " + famous.New.First().NewContent;
+                    }
+                    else
+                    {
+                        c2.ClueContent = dynProperty + " " + GetRandomCharacteristicSuspect(g.Suspect, cSuspect);
+                    }
                     characteristicsSuspect--;
                 }
                 else
                 {
-                    c2.ClueContent = cpd.CityPropertyContent;
+                    if (famous.New.Count() != 0 && famous.New.First() != null)
+                    {
+                        c2.ClueContent = dynProperty + " " + famous.New.First().NewContent;
+                    }
+                    else
+                    {
+                        c2.ClueContent = dynProperty;
+                    }
                 }
                 
                 /* build the second clue */
@@ -185,14 +243,31 @@ namespace InterpoolCloudWebRole.Controller
                 /* set de city */
                 c1.City = cnp.City;
                 /* if i have to put characteristics on the clue of the suspect */
+                string staticProperty = cps == null ? "" : cps.CityPropertyContent;
+                famous = cnp.Famous.ElementAt(2);
                 if (characteristicsSuspect != 0)
                 {
-                    c1.ClueContent = cps.CityPropertyContent + GetRandomCharacteristicSuspect(g.Suspect, cSuspect) + cnp.Famous.ElementAt(2).New;
+                    if (famous.New.Count() != 0 && famous.New.First().NewContent != null)
+                    {
+                        c1.ClueContent = staticProperty + " " + GetRandomCharacteristicSuspect(g.Suspect, cSuspect) + " " + famous.New.First().NewContent;
+                    }
+                    else
+                    {
+                        c2.ClueContent = staticProperty + " " + GetRandomCharacteristicSuspect(g.Suspect, cSuspect);
+                    }
                     characteristicsSuspect--;
-                }
+
+                  }
                 else
                 {
-                    c1.ClueContent = cps.CityPropertyContent;
+                    if (famous.New.Count() != 0 && famous.New.First() != null)
+                    {
+                        c1.ClueContent = staticProperty + " " + famous.New.First().NewContent;
+                    }
+                    else
+                    {
+                        c1.ClueContent = staticProperty;
+                    }
                 }
                 
                 /* Add clue for the NodePath in order*/
@@ -223,7 +298,7 @@ namespace InterpoolCloudWebRole.Controller
             return nextNodePath.First().City;
         }
 
-        private String GetRandomCharacteristicSuspect (Suspect s, List<Boolean> csuspect){
+        private String GetRandomCharacteristicSuspect (Suspect s, bool[] csuspect){
             /* get the random index for the characteristic of the suspect */
             Random rnd = new Random();
             int indexRandom = rnd.Next(0,5);
@@ -241,7 +316,16 @@ namespace InterpoolCloudWebRole.Controller
                     return s.SuspectPreferenceMovies;
                     
                 case 1:
-                    return s.SuspectPreferenceMovies;
+                    return s.SuspectPreferenceMusic;
+
+                case 2:
+                    return "Fanta definir la característica";
+
+                case 3:
+                    return "Fanta definir la característica";
+
+                case 4:
+                    return "Fanta definir la característica";
                 default:
                     return "";
             }
