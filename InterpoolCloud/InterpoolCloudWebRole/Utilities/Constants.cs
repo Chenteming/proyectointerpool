8ebff4b0d6b4819773d2f3e174d409ea79b2f9ba
vicenteacosta3@gmail.com <vicenteacosta3@gmail.com@ef7d9822-55ab-29b5-15d1-295a5a4f5466>
2010-09-25 15:54:52 +0000

    Agrego constante MAX_SUSPECTS

--- InterpoolCloud/InterpoolCloudWebRole/Utilities/Constants.cs
+++ InterpoolCloud/InterpoolCloudWebRole/Utilities/Constants.cs
@@ -8,5 +8,6 @@ namespace InterpoolCloudWebRole.Utilities
     public static class Constants
     {
         public static int NUMBERLASTCITY = 4;
+        public static int MAX_SUSPECTS = 10;
     }
 }
