b0c73f12ae3708e98ef1b4123e8938fb19e7ebf7
leticiavp <leticiavp@ef7d9822-55ab-29b5-15d1-295a5a4f5466>
2010-09-26 00:58:34 +0000

    Agrego botón para mostrar la pista del famoso y realizo unos retoques a la página

--- WP7/WP7/WP7/GamePages/Famous.xaml.cs
+++ WP7/WP7/WP7/GamePages/Famous.xaml.cs
@@ -23,73 +23,53 @@ namespace WP7
         public Famous()
         {
             InitializeComponent();
+            
+            // Change the language of the page
             language = LanguageManager.GetInstance();
             if (language.GetXDoc() != null)
                 language.TranslatePage(this);
-            GameManager gm = GameManager.getInstance();    
-        
+
+            GameManager gm = GameManager.getInstance();
             client = new ServiceWP7Client();
             client.GetCurrentFamousCompleted += new EventHandler<GetCurrentFamousCompletedEventArgs>(client_GetCurrentFamousCompleted);
             client.GetCurrentFamousAsync(gm.GetCurrentCity());
-
             client.CloseCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(client_CloseCompleted);
             client.CloseAsync();
             // Set the textboxes with the name of the famous
-            button1.Visibility = System.Windows.Visibility.Collapsed;
-                        
+            famousNameButton.Visibility = System.Windows.Visibility.Collapsed;                     
         }       
 		
 		public void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
 		{
 		}
 
-        private void Famous1_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-
-
         private void GetClueByFamousCallback(object sender, GetClueByFamousCompletedEventArgs e)
         {
             String clue = e.Result;
             dialogText.Text = clue;           
         }
-        void client_GetCurrentFamousCompleted(object sender, GetCurrentFamousCompletedEventArgs e)
-        {
-            GameManager gm = GameManager.getInstance();
-            gm.SetCurrentFamous(e.Result.ToList());            
-            List<String> famous = gm.GetFamous();
-
-            button1.Visibility = System.Windows.Visibility.Visible;
-
-
-            //Show in the textBoxes the name of the famous
-            button1.Content = famous.ElementAt(0);
-          
-
-            
-        }
 
         void client_CloseCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
         {
-            //TODO
         }
 
-        private void back_Click(object sender, RoutedEventArgs e)
+        void client_GetCurrentFamousCompleted(object sender, GetCurrentFamousCompletedEventArgs e)
         {
-            NavigationService.Navigate(new Uri("/GamePages/Game.xaml", UriKind.RelativeOrAbsolute));
+            GameManager gm = GameManager.getInstance();
+            gm.SetCurrentFamous(e.Result.ToList());
+            List<String> famous = gm.GetFamous();
+            famousNameButton.Visibility = System.Windows.Visibility.Visible;
+            //Show in the content of the button the name of the famous is going to be interrogated
+            famousNameButton.Content = famous.ElementAt(gm.GetCurrentFamous());
         }
 
-        private void button1_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-        	client = new ServiceWP7Client();
+        private void famousNameClick(object sender, RoutedEventArgs e)
+        {            
+            client = new ServiceWP7Client();
             client.GetClueByFamousCompleted += new EventHandler<GetClueByFamousCompletedEventArgs>(GetClueByFamousCallback);
-            client.GetClueByFamousAsync(button1.Content.ToString());
+            client.GetClueByFamousAsync(famousNameButton.Content.ToString());
             client.CloseCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(client_CloseCompleted);
             client.CloseAsync(); 
         }
-
-    
     }
 }
