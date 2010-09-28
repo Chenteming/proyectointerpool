469ab7448c37a8c77bf03fd29f718881380e61db
diegoem6@gmail.com <diegoem6@gmail.com@ef7d9822-55ab-29b5-15d1-295a5a4f5466>
2010-09-28 23:09:15 +0000

    actualización Diego

--- recursos/DataBase/parameters.sql
+++ recursos/DataBase/parameters.sql
@@ -1,2 +1,5 @@
 insert into [interpooldb].[dbo].[Parameters] (ParameterName, ParameterValue) values ('LEVEL_ROOKIE','ROOKIE');
 insert into [interpooldb].[dbo].[Parameters] (ParameterName, ParameterValue) values ('AMOUNT_CITIES','30');
+insert into [interpooldb].[dbo].[Parameters] (ParameterName, ParameterValue) values ('LAST_CLUE1_ESP','Me pareció verlo por aca');
+insert into [interpooldb].[dbo].[Parameters] (ParameterName, ParameterValue) values ('LAST_CLUE2_ESP','Me contarón que lo vieron por aca hace 5 minutos');
+insert into [interpooldb].[dbo].[Parameters] (ParameterName, ParameterValue) values ('LAST_CLUE3_ESP','Lo vi hace unos segundos');
