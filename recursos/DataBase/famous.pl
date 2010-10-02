c7c85884d2275039ba47975ce4a2d402ec4abae5
diegoem6@gmail.com <diegoem6@gmail.com@ef7d9822-55ab-29b5-15d1-295a5a4f5466>
2010-10-02 17:07:46 +0000

    sqls

--- recursos/DataBase/famous.pl
+++ recursos/DataBase/famous.pl
@@ -1,5 +1,5 @@
 while ( <> ){
 	if(/(.*)\$(.*)\$(.*)/){
-		print "insert into [interpooldb].[dbo].[CityPropertySet] FamousName, (City_CityId, NameFile) values (\'" . $2 . "\', (Select CityId FROM [interpooldb].[dbo].[Cities] where Lower(CityName) like Lower(\'$1\')), \'" . $3 . "\');" . "\n";
+		print "insert into [interpooldb].[dbo].[Famous] (FamousName, City_CityId, NameFileFamous) values (\'" . $2 . "\', (Select CityId FROM [interpooldb].[dbo].[Cities] where Lower(CityName) like Lower(\'$1\')), \'" . $3 . "\');" . "\n";
 	}
 }
