a39b4c85b88f0922980256d6efa7090f056e068e
vicenteacosta3@gmail.com <vicenteacosta3@gmail.com@ef7d9822-55ab-29b5-15d1-295a5a4f5466>
2010-10-03 22:51:53 +0000

    Arreglo error, no tomaba el return de la operacion de filtrado. Se agrega ademas imagenes dinamicas

--- WP7/WP7/WP7/GamePages/Suspect.xaml.cs
+++ WP7/WP7/WP7/GamePages/Suspect.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using WP7.ServiceReference;
+using System.Windows.Media.Imaging;
 
 namespace WP7
 {
@@ -20,7 +21,7 @@ namespace WP7
         private static int index;
 		private LanguageManager language;
 		private GameManager gm = GameManager.getInstance();
-        private List<DataFacebookUser> dfu = new List<DataFacebookUser>();
+        private List<DataFacebookUser> dfbuList = new List<DataFacebookUser>();
 
         public Suspect()
         {
@@ -30,7 +31,7 @@ namespace WP7
             if (language.GetXDoc() != null)
                 language.TranslatePage(this);
 			InterpoolWP7Client client = new InterpoolWP7Client();
-            DataFacebookUser dfu = new DataFacebookUser();
+            DataFacebookUser dfbu = new DataFacebookUser();
             /*0 = first_name
               1 = last_name 
               2 =  birthday
@@ -39,15 +40,15 @@ namespace WP7
               5 = music
               6 = cinema*/
             string[] filterField = gm.GetFilterField();
-            dfu.first_name = filterField[0];
-            dfu.last_name = filterField[1];
-            dfu.birthday = filterField[2];
-            dfu.hometown = filterField[3];
-            dfu.gender = filterField[4];
-            dfu.music = filterField[5];
-            dfu.cinema = filterField[6];
+            dfbu.first_name = filterField[0];
+            dfbu.last_name = filterField[1];
+            dfbu.birthday = filterField[2];
+            dfbu.hometown = filterField[3];
+            dfbu.gender = filterField[4];
+            dfbu.music = filterField[5];
+            dfbu.cinema = filterField[6];
             client.FilterSuspectsCompleted += new EventHandler<FilterSuspectsCompletedEventArgs>(client_FilterSuspectsCompleted);
-            client.FilterSuspectsAsync(gm.userId, dfu);
+            client.FilterSuspectsAsync(gm.userId, dfbu);
             client.CloseCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(client_CloseCompleted);
             client.CloseAsync();
         }
@@ -60,60 +61,80 @@ namespace WP7
         void client_FilterSuspectsCompleted(object sender, FilterSuspectsCompletedEventArgs e)
         {
             index = 0;
-            if (dfu.Count == 0)
+            dfbuList = e.Result.ToList();
+            if (dfbuList.Count == 0)
                 Name_Suspect.Text = "There are no suspects";
             else
-                {                    
-                    dfu = e.Result.ToList();
-                    Name_Suspect.Text = dfu.ElementAt(0).first_name + dfu.ElementAt(0).last_name;
-                    hometown.Text = dfu.ElementAt(0).hometown;
-                    birthdayTB.Text = dfu.ElementAt(0).birthday;
-                    hometownTB.Text = dfu.ElementAt(0).hometown;
-                    genderTB.Text = dfu.ElementAt(0).gender;
-                    musicTB.Text = dfu.ElementAt(0).music;
-                    cinemaTB.Text = dfu.ElementAt(0).cinema;
-                }
+            {                    
+                dfbuList = e.Result.ToList();
+                Name_Suspect.Text = dfbuList.ElementAt(0).first_name + dfbuList.ElementAt(0).last_name;
+                hometown.Text = dfbuList.ElementAt(0).hometown;
+                birthdayTB.Text = dfbuList.ElementAt(0).birthday;
+                hometownTB.Text = dfbuList.ElementAt(0).hometown;
+                genderTB.Text = dfbuList.ElementAt(0).gender;
+                musicTB.Text = dfbuList.ElementAt(0).music;
+                cinemaTB.Text = dfbuList.ElementAt(0).cinema;
+
+                LoadPicture(dfbuList.ElementAt(0).pictureLink);
+            }
         }
-    
+
         private void LeftArrow1_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            if (dfu.Count == 0)
+            if (dfbuList.Count == 0)
                 Name_Suspect.Text = "There are no suspects";
             else
             {
                 if (index == 0)
-                    index = dfu.Count - 1;
+                    index = dfbuList.Count - 1;
                 else
                     index--;
 
-                Name_Suspect.Text = dfu.ElementAt(index).first_name + dfu.ElementAt(index).last_name;
-                birthdayTB.Text = dfu.ElementAt(index).birthday;
-                hometownTB.Text = dfu.ElementAt(index).hometown;
-                genderTB.Text = dfu.ElementAt(index).gender;
-                musicTB.Text = dfu.ElementAt(index).music;
-                cinemaTB.Text = dfu.ElementAt(index).cinema;
+                Name_Suspect.Text = dfbuList.ElementAt(index).first_name + dfbuList.ElementAt(index).last_name;
+                birthdayTB.Text = dfbuList.ElementAt(index).birthday;
+                hometownTB.Text = dfbuList.ElementAt(index).hometown;
+                genderTB.Text = dfbuList.ElementAt(index).gender;
+                musicTB.Text = dfbuList.ElementAt(index).music;
+                cinemaTB.Text = dfbuList.ElementAt(index).cinema;
 
+                LoadPicture(dfbuList.ElementAt(index).pictureLink);
             }
         }
 
         private void RightArrow1_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            if (dfu.Count == 0)
+            if (dfbuList.Count == 0)
                 Name_Suspect.Text = "There are no suspects";
             else
             {
-                if (index == dfu.Count - 1)
+                if (index == dfbuList.Count - 1)
                     index = 0;
                 else
                     index++;
 
-                Name_Suspect.Text = dfu.ElementAt(index).first_name + dfu.ElementAt(index).last_name;
-                birthdayTB.Text = dfu.ElementAt(index).birthday;
-                hometownTB.Text = dfu.ElementAt(index).hometown;
-                genderTB.Text = dfu.ElementAt(index).gender;
-                musicTB.Text = dfu.ElementAt(index).music;
-                cinemaTB.Text = dfu.ElementAt(index).cinema;
+                Name_Suspect.Text = dfbuList.ElementAt(index).first_name + dfbuList.ElementAt(index).last_name;
+                birthdayTB.Text = dfbuList.ElementAt(index).birthday;
+                hometownTB.Text = dfbuList.ElementAt(index).hometown;
+                genderTB.Text = dfbuList.ElementAt(index).gender;
+                musicTB.Text = dfbuList.ElementAt(index).music;
+                cinemaTB.Text = dfbuList.ElementAt(index).cinema;
+
+                LoadPicture(dfbuList.ElementAt(index).pictureLink);
             }
+        }
+
+        private void LoadPicture(string pictureLink)
+        {
+            WebClient webClientImgDownloader = new WebClient();
+            webClientImgDownloader.OpenReadCompleted += new OpenReadCompletedEventHandler(webClientImgDownloader_OpenReadCompleted);
+            webClientImgDownloader.OpenReadAsync(new Uri(pictureLink, UriKind.Absolute));
+        }
+
+        void webClientImgDownloader_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e)
+        {
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.SetSource(e.Result);
+            imageSuspect.Source = bitmap;
         }
     }
 }
