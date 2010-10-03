3c7351fa9d3201c467c8b9ef8a9493f14eb70768
mtincho12 <mtincho12@ef7d9822-55ab-29b5-15d1-295a5a4f5466>
2010-10-03 16:59:23 +0000

    Lety

--- trunk/WP7/WP7/WP7/GamePages/Filter.xaml.cs
+++ trunk/WP7/WP7/WP7/GamePages/Filter.xaml.cs
@@ -10,16 +10,20 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using WP7.ServiceReference;
 
 namespace WP7.GamePages
 {
-    public partial class PagePruv : PhoneApplicationPage
+    public partial class Filter : PhoneApplicationPage
     {
 		private List<String> gender;
 		private List<String> homeTown;
 		private List<String> film;
 		private List<String> music;
-        public PagePruv()
+        private InterpoolWP7Client client = new InterpoolWP7Client();
+        private GameManager gm = GameManager.getInstance();
+
+        public Filter()
         {			
             InitializeComponent();
 			comboHomeTown.Visibility = System.Windows.Visibility.Collapsed;
@@ -27,64 +31,52 @@ namespace WP7.GamePages
 			comboMusic.Visibility = System.Windows.Visibility.Collapsed;
 			comboGender.Visibility = System.Windows.Visibility.Collapsed;
             FilterButton.Visibility = System.Windows.Visibility.Collapsed;
-			
+
+            client.FilterSuspectsCompleted += new EventHandler<FilterSuspectsCompletedEventArgs>(client_FilterSuspectsCompleted);
+            DataFacebookUser dfu = new DataFacebookUser();
+            client.FilterSuspectsAsync(gm.userId, dfu);
+
 			// Setting comboGender
-			gender = new List<String>();
-			gender.Add("Masculino");			
-			gender.Add("Femenino");
-			comboGender.ItemsSource = gender;
+		   /* gender = new List<String>();
+		    gender.Add("Masculino");			
+		    gender.Add("Femenino");
+		    comboGender.ItemsSource = gender;
 			
-			homeTown = new List<String>();
-			homeTown.Add("Masculino");			
-			homeTown.Add("Femenino");
-			comboHomeTown.ItemsSource = homeTown;
+		    homeTown = new List<String>();
+		    homeTown.Add("Masculino");			
+		    homeTown.Add("Femenino");
+		    comboHomeTown.ItemsSource = homeTown;
 			
-			film = new List<String>();
-			film.Add("MATRIX");			
-			film.Add("EXTERMINIO");
-			comboFilm.ItemsSource = film;
+		    film = new List<String>();
+		    film.Add("MATRIX");			
+		    film.Add("EXTERMINIO");
+		    comboFilm.ItemsSource = film;
 			
-			music = new List<String>();
-			music.Add("ROCK");			
-			music.Add("POP");
-			comboMusic.ItemsSource = music;	
-			
-//			// Setting comboHomeTown
-//			cboxitem = new ComboBoxItem();
-//			cboxitem.Content = "Montevideo";
-//			comboHomeTown.Items.Add(cboxitem);
-//			cboxitem2 = new ComboBoxItem();
-//			cboxitem2.Content = "Rocha";
-//			comboHomeTown.Items.Add(cboxitem2);
-//
-//			// Setting comboFilm
-//			cboxitem = new ComboBoxItem();
-//			cboxitem.Content = "Matrix";
-//			comboFilm.Items.Add(cboxitem);
-//			cboxitem2 = new ComboBoxItem();
-//			cboxitem2.Content = "Jurasic Park";
-//			comboFilm.Items.Add(cboxitem2);
-//			cboxitem3 = new ComboBoxItem();
-//			cboxitem3.Content = "Sponge Bob";
-//			comboFilm.Items.Add(cboxitem3);
-//			cboxitem4 = new ComboBoxItem();
-//			cboxitem4.Content = "The saw";
-//			comboFilm.Items.Add(cboxitem4);
-//			
-//			// Setting comboMusic
-//			cboxitem = new ComboBoxItem();
-//			cboxitem.Content = "No te va gustar";
-//			comboMusic.Items.Add(cboxitem);
-//			cboxitem2 = new ComboBoxItem();
-//			cboxitem2.Content = "Redondos";
-//			comboMusic.Items.Add(cboxitem2);
-//			cboxitem3 = new ComboBoxItem();
-//			cboxitem3.Content = "Beatles";
-//			comboMusic.Items.Add(cboxitem3);
-//			cboxitem4 = new ComboBoxItem();
-//			cboxitem4.Content = "Mana";
-//			comboMusic.Items.Add(cboxitem4);
-//			
+		    music = new List<String>();
+		    music.Add("ROCK");			
+		    music.Add("POP");
+		    comboMusic.ItemsSource = music;	*/
+        }
+
+        void client_FilterSuspectsCompleted(object sender, FilterSuspectsCompletedEventArgs e)
+        {
+            List<DataFacebookUser> dfu = e.Result.ToList();
+            gender = new List<String>();
+            film = new List<String>();
+            homeTown = new List<String>();
+            music = new List<String>();
+            // TODO chequear repetidos no se agregen
+            foreach(DataFacebookUser df in dfu) 
+            {
+                film.Add(df.cinema);
+                gender.Add(df.gender);
+                homeTown.Add(df.hometown);
+                music.Add(df.music);
+            }                     
+            comboGender.ItemsSource = gender;
+            comboHomeTown.ItemsSource = homeTown;
+            comboFilm.ItemsSource = film;
+            comboMusic.ItemsSource = music;	
         }
 
         private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
