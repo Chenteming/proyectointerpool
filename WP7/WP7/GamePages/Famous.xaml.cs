d6c058d27b01a6f7309ae737daa3c2145bfa4dab
vicenteacosta3@gmail.com <vicenteacosta3@gmail.com@ef7d9822-55ab-29b5-15d1-295a5a4f5466>
2010-10-04 21:01:06 +0000

    Tagueo la version actual del producto.

---
+++ WP7/WP7/GamePages/Famous.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using WP7.ServiceReference;
+
+
+namespace WP7
+{
+    public partial class Famous : PhoneApplicationPage
+    {
+        private InterpoolWP7Client client;
+        private LanguageManager language = LanguageManager.GetInstance();
+        private GameManager gm = GameManager.getInstance();
+
+        public Famous()
+        {
+            InitializeComponent();
+            
+            // Change the language of the page            
+            if (language.GetXDoc() != null)
+                language.TranslatePage(this);
+			//famousImage.Source = "/WP7;component/FamousImages/Lety.jpg";
+            client = new InterpoolWP7Client();
+            client.GetCurrentFamousCompleted += new EventHandler<GetCurrentFamousCompletedEventArgs>(client_GetCurrentFamousCompleted);
+            client.GetCurrentFamousAsync(gm.userId, gm.GetCurrentFamous() - 1);
+            client.CloseCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(client_CloseCompleted);
+            client.CloseAsync();
+            // Set the textboxes with the name of the famous
+            famousNameButton.Visibility = System.Windows.Visibility.Collapsed;                     
+        }       
+		
+		public void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+		{
+		}
+
+        private void GetClueByFamousCallback(object sender, GetClueByFamousCompletedEventArgs e)
+        {
+            DataClue dc = e.Result;
+            dialogText.Text = dc.clue;
+            switch (dc.state)
+            {
+                case DataClue.State.LOSE_EOAW:
+                    MessageBox.Show("Haz emitido la orden de arresto de forma incorrecta.");
+                    NavigationService.Navigate(new Uri("MainPage.xaml", UriKind.RelativeOrAbsolute));
+                    break;
+                case DataClue.State.LOSE_NEOA:
+                    MessageBox.Show("No haz emitido una orden de arresto.");
+                    NavigationService.Navigate(new Uri("MainPage.xaml", UriKind.RelativeOrAbsolute));
+                    break;
+                case DataClue.State.WIN:
+                    MessageBox.Show("Ganaste!!!!!!!");
+                    NavigationService.Navigate(new Uri("MainPage.xaml", UriKind.RelativeOrAbsolute));
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        void client_CloseCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+        }
+
+        void client_GetCurrentFamousCompleted(object sender, GetCurrentFamousCompletedEventArgs e)
+        {
+            DataFamous dataF = e.Result;
+            int num = gm.GetCurrentFamous();
+            gm.AddFamous(num-1, dataF.nameFamous);
+            famousNameButton.Visibility = System.Windows.Visibility.Visible;
+            //Show in the content of the button the name of the famous is going to be interrogated
+            famousNameButton.Content = dataF.nameFamous;
+        }
+
+        private void famousNameClick(object sender, RoutedEventArgs e)
+        {            
+            client = new InterpoolWP7Client();
+            client.GetClueByFamousCompleted += new EventHandler<GetClueByFamousCompletedEventArgs>(GetClueByFamousCallback);
+            client.GetClueByFamousAsync(gm.userId, gm.GetCurrentFamous() - 1);
+            client.CloseCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(client_CloseCompleted);
+            client.CloseAsync(); 
+        }
+    }
+}
---
+++ WP7/WP7/GamePages/Filter.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using WP7.ServiceReference;
+
+namespace WP7.GamePages
+{
+    public partial class Filter : PhoneApplicationPage
+    {
+		private List<String> gender;
+		private List<String> homeTown;
+		private List<String> film;
+		private List<String> music;
+        private InterpoolWP7Client client = new InterpoolWP7Client();
+        private GameManager gm = GameManager.getInstance();
+
+        public Filter()
+        {			
+            InitializeComponent();
+			comboHomeTown.Visibility = System.Windows.Visibility.Collapsed;
+			comboFilm.Visibility = System.Windows.Visibility.Collapsed;
+			comboMusic.Visibility = System.Windows.Visibility.Collapsed;
+			comboGender.Visibility = System.Windows.Visibility.Collapsed;
+            FilterButton.Visibility = System.Windows.Visibility.Visible;
+            client.FilterSuspectsCompleted += new EventHandler<FilterSuspectsCompletedEventArgs>(client_FilterSuspectsCompleted);
+            DataFacebookUser dfu = new DataFacebookUser();
+            client.FilterSuspectsAsync(gm.userId, dfu);
+            client.CloseCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(client_CloseCompleted);
+            client.CloseAsync();
+        }
+
+        void client_CloseCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+           
+        }
+
+        void client_FilterSuspectsCompleted(object sender, FilterSuspectsCompletedEventArgs e)
+        {
+            List<DataFacebookUser> dfu = e.Result.ToList();
+            gender = new List<String>();
+            film = new List<String>();
+            homeTown = new List<String>();
+            music = new List<String>();
+            foreach(DataFacebookUser df in dfu) 
+            {
+                if (!film.Contains(df.cinema))
+                    film.Add(df.cinema);
+                if (!gender.Contains(df.gender))
+                    gender.Add(df.gender);
+                if (!homeTown.Contains(df.hometown))
+                    homeTown.Add(df.hometown);
+                if (!music.Contains(df.music))
+                    music.Add(df.music);
+            }                     
+            comboGender.ItemsSource = gender;
+            comboHomeTown.ItemsSource = homeTown;
+            comboFilm.ItemsSource = film;
+            comboMusic.ItemsSource = music;	
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	//FilterSuspectsStoryboard.Begin();
+			comboHomeTown.Visibility = System.Windows.Visibility.Visible;
+			comboFilm.Visibility = System.Windows.Visibility.Visible;
+			comboMusic.Visibility = System.Windows.Visibility.Visible;
+			comboGender.Visibility = System.Windows.Visibility.Visible;
+            OpenButton.Visibility = System.Windows.Visibility.Collapsed;
+            OpenButton.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void FilterButton_Click(object sender, RoutedEventArgs e)
+        {
+            string[] filterField = gm.GetFilterField();
+            /*0 = first_name
+              1 = last_name 
+              2 =  birthday
+              3 = hometown
+              4 = gender
+              5 = music
+              6 = cinema*/
+            if (comboHomeTown.SelectedItem != null)
+                 filterField[3] = comboHomeTown.SelectedItem.ToString();
+            else
+                filterField[3] = "";
+            if (comboGender.SelectedItem != null)
+                filterField[4] = comboGender.SelectedItem.ToString();
+            else
+                filterField[4] = "";
+            if (comboMusic.SelectedItem != null)
+                filterField[5] = comboMusic.SelectedItem.ToString();
+            else
+                filterField[5] = "";
+            if (comboFilm.SelectedItem != null)
+                filterField[6] = comboFilm.SelectedItem.ToString();
+            else
+                filterField[6] = "";
+            NavigationService.Navigate(new Uri("/GamePages/Suspect.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
