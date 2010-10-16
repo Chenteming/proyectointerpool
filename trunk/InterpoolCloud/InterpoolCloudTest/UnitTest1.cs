e5e21579fd55bbd75a92fb812f3fd83daae6ee76
mtincho12 <mtincho12@ef7d9822-55ab-29b5-15d1-295a5a4f5466>
2010-10-16 17:00:45 +0000

    Proyecto para test

---
+++ trunk/InterpoolCloud/InterpoolCloudTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InterpoolCloudWebRole.Data;
+namespace InterpoolCloudTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private InterpoolContainer container = new InterpoolContainer(@"metadata=res://*/Data.InterpoolModel.csdl|res://*/Data.InterpoolModel.ssdl|res://*/Data.InterpoolModel.msl; provider=System.Data.SqlClient; ;provider connection string='Data Source=MARTIN-PC\SQLEXPRESS;Initial Catalog=InterpoolDB;Integrated Security=True;MultipleActiveResultSets=True'");
+        private DataManager dm = new DataManager();
+
+       
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            List<City> cities = dm.getCities(container).ToList();
+            Assert.AreEqual(cities.Count, 32);
+        }
+    }
+}
---
+++ trunk/InterpoolCloud/InterpoolCloudWebRole/Tests/ArmarIteraccionTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InterpoolCloudWebRole.Tests
+{
+    public class ArmarIteraccionTest
+    {
+    }
+}
