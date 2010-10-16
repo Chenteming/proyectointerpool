b29ed2289a65ceffa6d38b9bd121e4daf0664587
nachopando1986 <nachopando1986@ef7d9822-55ab-29b5-15d1-295a5a4f5466>
2010-10-16 21:58:19 +0000

    Modificación de las clases, los "using" están todos en dentro de los namespaces.

--- InterpoolCloud/InterpoolCloudWebRole/Data/IDataManager.cs
+++ InterpoolCloud/InterpoolCloudWebRole/Data/IDataManager.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using InterpoolCloudWebRole.FacebookCommunication;
-using InterpoolCloudWebRole.Datatypes;
-
+﻿
 namespace InterpoolCloudWebRole.Data
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using InterpoolCloudWebRole.FacebookCommunication;
+    using InterpoolCloudWebRole.Datatypes;
+
     interface IDataManager
     {
         IQueryable<City> getCities(InterpoolContainer context);
