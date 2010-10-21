4138b32add6929147f1256738c28ff2ab2870e8b
vicenteacosta3@gmail.com <vicenteacosta3@gmail.com@ef7d9822-55ab-29b5-15d1-295a5a4f5466>
2010-10-21 01:40:22 +0000

    Tagueo el codigo de la semana 10 de WP7.

---
+++ tags/WP7_10_0/WP7/WP7/GamePages/Filter.xaml.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using WP7.ServiceReference;
+using WP7.Utilities;
+
+namespace WP7.GamePages
+{
+    public partial class Filter : PhoneApplicationPage
+    {
+		private List<String> gender;
+		private List<String> homeTown;
+		private List<String> film;
+		private List<String> music;
+        private List<String> tv;
+        private string[] filters;
+        private InterpoolWP7Client client = new InterpoolWP7Client();
+        private GameManager gm = GameManager.getInstance();
+		private LanguageManager language = LanguageManager.GetInstance();
+        int btnPosition = 0;
+		
+        public Filter()
+        {			
+            InitializeComponent();
+			AnimationPage.Begin();
+			// Change the language of the page            
+            if (language.GetXDoc() != null)
+                language.TranslatePage(this);	
+            FilterButton.Visibility = System.Windows.Visibility.Collapsed;
+            client.FilterSuspectsCompleted += new EventHandler<FilterSuspectsCompletedEventArgs>(client_FilterSuspectsCompleted);
+            DataFacebookUser dfu = new DataFacebookUser();
+            client.FilterSuspectsAsync(gm.userId, dfu);
+            client.CloseCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(client_CloseCompleted);
+            client.CloseAsync();
+            filters = new string[Constants.MAX_FILTERFIELD];
+        }
+
+        void client_CloseCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+           
+        }
+
+        void client_FilterSuspectsCompleted(object sender, FilterSuspectsCompletedEventArgs e)
+        {
+            List<DataFacebookUser> dfu = e.Result.ToList();
+            gender = new List<String>();
+            film = new List<String>();
+            homeTown = new List<String>();
+            music = new List<String>();
+			tv = new List<String>();
+            foreach(DataFacebookUser df in dfu) 
+            {
+                if (!film.Contains(df.cinema))
+                    film.Add(df.cinema);
+                if (!gender.Contains(df.gender))
+                    gender.Add(df.gender);
+                if (!homeTown.Contains(df.hometown))
+                    homeTown.Add(df.hometown);
+                if (!music.Contains(df.music))
+                    music.Add(df.music);
+				if (!tv.Contains(df.television))
+                    tv.Add(df.television);
+            }
+        }
+
+        /*private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	//FilterSuspectsStoryboard.Begin();
+			comboHomeTown.Visibility = System.Windows.Visibility.Visible;
+			comboFilm.Visibility = System.Windows.Visibility.Visible;
+			comboMusic.Visibility = System.Windows.Visibility.Visible;
+			comboGender.Visibility = System.Windows.Visibility.Visible;
+			comboTV.Visibility = System.Windows.Visibility.Visible;
+            OpenButton.Visibility = System.Windows.Visibility.Collapsed;
+
+            string[] filterField = gm.GetFilterField();			
+			comboHomeTown.SelectedItem = filterField[3];
+			comboGender.SelectedItem = filterField[4];
+			comboMusic.SelectedItem = filterField[5];
+			comboFilm.SelectedItem = filterField[6];
+			comboTV.SelectedItem = filterField[7];
+			FilterButton.Visibility = System.Windows.Visibility.Visible;
+        }*/
+
+        private void FilterButton_Click(object sender, RoutedEventArgs e)
+        {
+            string[] filterField = gm.GetFilterField();
+            for (int i = 0; i < 8; i++) 
+            {
+                filterField[i] = filters[i];
+            }                
+            NavigationService.Navigate(new Uri("/GamePages/Suspect.xaml", UriKind.RelativeOrAbsolute));
+        }       
+
+        private void ComboList_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (ComboList.SelectedIndex != -1)
+            {
+                filters[btnPosition] = ComboList.SelectedItem.ToString();
+                ComboList.Visibility = Visibility.Collapsed;
+                ContentGrid2.Visibility = Visibility.Collapsed;
+                ContentGrid.Visibility = Visibility.Visible;
+            }
+        }        
+
+
+        private void TVButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	btnPosition = 7;
+            string[] filterField = gm.GetFilterField();
+            ComboList.ItemsSource = tv;
+            ContentGrid.Visibility = Visibility.Collapsed;
+            ContentGrid2.Visibility = Visibility.Visible;
+            ComboList.Visibility = Visibility.Visible;
+        }
+
+        private void CinemaButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	btnPosition = 6;
+            string[] filterField = gm.GetFilterField();
+            ComboList.ItemsSource = film;
+            ContentGrid.Visibility = Visibility.Collapsed;
+            ContentGrid2.Visibility = Visibility.Visible;
+            ComboList.Visibility = Visibility.Visible;
+        }
+
+        private void HometownButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	btnPosition = 3;
+            string[] filterField = gm.GetFilterField();
+            ComboList.ItemsSource = homeTown;
+            ContentGrid.Visibility = Visibility.Collapsed;
+            ContentGrid2.Visibility = Visibility.Visible;
+            ComboList.Visibility = Visibility.Visible;
+        }
+
+        private void BirthdayButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	// TODO: Add event handler implementation here.
+        }
+
+        private void GenderButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            btnPosition = 4;
+            string[] filterField = gm.GetFilterField();
+            ComboList.ItemsSource = gender;
+            ContentGrid.Visibility = Visibility.Collapsed;
+            ContentGrid2.Visibility = Visibility.Visible;
+            ComboList.Visibility = Visibility.Visible;
+        }
+
+        private void MusicButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+ 			btnPosition = 5;
+            string[] filterField = gm.GetFilterField();
+            ComboList.ItemsSource = music;
+            ContentGrid.Visibility = Visibility.Collapsed;
+            ContentGrid2.Visibility = Visibility.Visible;
+            ComboList.Visibility = Visibility.Visible;
+        }
+
+            
+    }
+}
---
+++ tags/WP7_10_0/WP7/WP7/GamePages/Suspect.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using WP7.ServiceReference;
+using System.Windows.Media.Imaging;
+
+namespace WP7
+{
+    public partial class Suspect : PhoneApplicationPage
+    {
+        private static int index;
+		private LanguageManager language;
+		private GameManager gm = GameManager.getInstance();
+        private List<DataFacebookUser> dfbuList = new List<DataFacebookUser>();
+
+        public Suspect()
+        {
+            InitializeComponent();
+			AnimationPage.Begin();
+			// Change the language of the page
+            language = LanguageManager.GetInstance();
+            if (language.GetXDoc() != null)
+                language.TranslatePage(this);
+			InterpoolWP7Client client = new InterpoolWP7Client();
+            DataFacebookUser dfbu = new DataFacebookUser();
+            /*0 = first_name
+              1 = last_name 
+              2 =  birthday
+              3 = hometown
+              4 = gender
+              5 = music
+              6 = cinema
+			  7 = television*/
+            string[] filterField = gm.GetFilterField();
+            dfbu.first_name = filterField[0];
+            dfbu.last_name = filterField[1];
+            dfbu.birthday = filterField[2];
+            dfbu.hometown = filterField[3];
+            dfbu.gender = filterField[4];
+            dfbu.music = filterField[5];
+            dfbu.cinema = filterField[6];
+			dfbu.television = filterField[7];
+            client.FilterSuspectsCompleted += new EventHandler<FilterSuspectsCompletedEventArgs>(client_FilterSuspectsCompleted);
+            client.FilterSuspectsAsync(gm.userId, dfbu);
+            client.CloseCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(client_CloseCompleted);
+            client.CloseAsync();
+        }
+
+        void client_CloseCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            
+        }
+
+        void client_FilterSuspectsCompleted(object sender, FilterSuspectsCompletedEventArgs e)
+        {
+            index = 0;
+            dfbuList = e.Result.ToList();
+            if (dfbuList.Count == 0)
+
+                Name_Suspect.Text = "There are no suspects";
+            else
+            {                    
+                dfbuList = e.Result.ToList();
+                Name_Suspect.Text = dfbuList.ElementAt(0).first_name + " " + dfbuList.ElementAt(0).last_name;
+                hometownTB.Text = dfbuList.ElementAt(0).hometown;
+                birthdayTB.Text = dfbuList.ElementAt(0).birthday;
+                hometownTB.Text = dfbuList.ElementAt(0).hometown;
+                genderTB.Text = dfbuList.ElementAt(0).gender;
+                musicTB.Text = dfbuList.ElementAt(0).music;
+                cinemaTB.Text = dfbuList.ElementAt(0).cinema;
+				televisionTB.Text = dfbuList.ElementAt(0).television;
+                LoadPicture(dfbuList.ElementAt(0).pictureLink);
+            }
+        }
+
+        private void LeftArrow1_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (dfbuList.Count == 0)
+                Name_Suspect.Text = "There are no suspects";
+            else
+            {
+                if (index == 0)
+                    index = dfbuList.Count - 1;
+                else
+                    index--;
+
+                Name_Suspect.Text = dfbuList.ElementAt(index).first_name + " " + dfbuList.ElementAt(index).last_name;
+                birthdayTB.Text = dfbuList.ElementAt(index).birthday;
+                hometownTB.Text = dfbuList.ElementAt(index).hometown;
+                genderTB.Text = dfbuList.ElementAt(index).gender;
+                musicTB.Text = dfbuList.ElementAt(index).music;
+                cinemaTB.Text = dfbuList.ElementAt(index).cinema;
+				televisionTB.Text = dfbuList.ElementAt(index).television;
+                LoadPicture(dfbuList.ElementAt(index).pictureLink);
+            }
+        }
+
+        private void RightArrow1_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (dfbuList.Count == 0)
+                Name_Suspect.Text = "There are no suspects";
+            else
+            {
+                if (index == dfbuList.Count - 1)
+                    index = 0;
+                else
+                    index++;
+
+                Name_Suspect.Text = dfbuList.ElementAt(index).first_name + " " + dfbuList.ElementAt(index).last_name;
+                birthdayTB.Text = dfbuList.ElementAt(index).birthday;
+                hometownTB.Text = dfbuList.ElementAt(index).hometown;
+                genderTB.Text = dfbuList.ElementAt(index).gender;
+                musicTB.Text = dfbuList.ElementAt(index).music;
+                cinemaTB.Text = dfbuList.ElementAt(index).cinema;
+				televisionTB.Text = dfbuList.ElementAt(index).television;
+                LoadPicture(dfbuList.ElementAt(index).pictureLink);
+            }
+        }
+
+        private void LoadPicture(string pictureLink)
+        {
+            WebClient webClientImgDownloader = new WebClient();
+            webClientImgDownloader.OpenReadCompleted += new OpenReadCompletedEventHandler(webClientImgDownloader_OpenReadCompleted);
+            webClientImgDownloader.OpenReadAsync(new Uri(pictureLink, UriKind.Absolute));
+        }
+
+        void webClientImgDownloader_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e)
+        {
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.SetSource(e.Result);
+            imageSuspect.Source = bitmap;
+        }
+
+        private void Emit_Click(object sender, RoutedEventArgs e)
+        {
+            InterpoolWP7Client client = new InterpoolWP7Client();
+            client.EmitOrderOfArrestCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(client_EmitOrderOfArrestCompleted);
+            client.EmitOrderOfArrestAsync(gm.userId, dfbuList.ElementAt(index).id_friend);
+            client.CloseCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(client_CloseCompleted);
+            client.CloseAsync();
+            Emit.IsEnabled = false;
+            MessageBox.Show("Se ha emitido una orden de arresto para " + Name_Suspect.Text);
+            NavigationService.Navigate(new Uri("/GamePages/Game.xaml", UriKind.RelativeOrAbsolute));            
+        }
+
+        void client_EmitOrderOfArrestCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+        }
+
+        private void Return_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	NavigationService.Navigate(new Uri("/GamePages/Game.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
