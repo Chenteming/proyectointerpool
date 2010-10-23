4e328f4f99515eeab0ae4fe35973aa98b15c59ef
fandrade9@gmail.com <fandrade9@gmail.com@ef7d9822-55ab-29b5-15d1-295a5a4f5466>
2010-10-23 23:26:02 +0000

    nueva clase que se utilizará para poner funciones de uso general

---
+++ InterpoolCloud/InterpoolCloudWebRole/Utilities/Functions.cs
@@ -0,0 +1,46 @@
+﻿
+////-----------------------------------------------------------------------
+////<copyright file="Functions.cs" company="Interpool">
+////     Copyright Interpool. All rights reserved.
+//// </copyright>
+////-----------------------------------------------------------------------
+namespace InterpoolCloudWebRole.Utilities
+{
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+    /// <summary>
+    /// Class statement Functions
+    /// </summary>    
+    public class Functions
+    {
+
+        /// <summary>
+        /// Description for Method.</summary>
+        /// <param name="noticia"> Parameter description for inputList goes here</param>
+        /// <returns>
+        /// Return results are described through the returns tag.</returns>
+        public Functions()
+        { 
+        }
+        /// <summary>
+        /// Description for Method.</summary>
+        /// <param name="noticia"> Parameter description for inputList goes here</param>
+        /// <returns>
+        /// Return results are described through the returns tag.</returns>
+        public List<E> SuffleList<E>(List<E> inputList)
+        {
+            List<E> randomList = new List<E>();
+            Random r = new Random();
+            int randomIndex = 0;
+            while (inputList.Count > 0)
+            {
+                randomIndex = r.Next(0, inputList.Count); ////Choose a random object in the list 
+                randomList.Add(inputList[randomIndex]); ////add it to the new, random list 
+                inputList.RemoveAt(randomIndex); ////remove to avoid duplicates 
+            }
+            return randomList; ////return the new random list
+        }
+    }
+}
