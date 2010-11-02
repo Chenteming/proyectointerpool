3d6a3161d877f415df7f05fb9696f1c0471f45a1
mtincho12 <mtincho12@ef7d9822-55ab-29b5-15d1-295a5a4f5466>
2010-11-02 21:58:22 +0000

    Info sobre el fin del juego

---
+++ InterpoolCloud/InterpoolCloudWebRole/Datatypes/DataGameInfo.cs
@@ -0,0 +1,25 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DataGameInfo.cs" company="Interpool">
+//     Copyright Interpool. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace InterpoolCloudWebRole.Datatypes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public class DataGameInfo
+    {
+        public string SuspectName {get; set;}
+        public int DiffInDays {get; set;}
+        public int DiffInMinutes { get; set; }
+        public int DiffInseconds { get; set; }
+        public long ScoreWin {get; set;}
+        public long Score {get; set;}
+        public string LinkBigSuspect {get; set;}
+        public string newLevel { get; set; }
+
+    }
+}
