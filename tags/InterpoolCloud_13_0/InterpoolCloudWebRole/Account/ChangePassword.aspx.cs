3ee666956c1739fbddd9b53e74bcc4e815ada6d8
vicenteacosta3@gmail.com <vicenteacosta3@gmail.com@ef7d9822-55ab-29b5-15d1-295a5a4f5466>
2010-11-03 14:51:14 +0000

    Abro tag para la semana 13

---
+++ tags/InterpoolCloud_13_0/InterpoolCloudWebRole/Account/ChangePassword.aspx.cs
@@ -0,0 +1,28 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ChangePassword.aspx.cs" company="Interpool">
+//     Copyright Interpool. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace InterpoolCloudWebRole.Account
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.UI;
+    using System.Web.UI.WebControls;
+
+    /// <summary>
+    /// Partial class declaration ChangePassword
+    /// </summary>
+    public partial class ChangePassword : System.Web.UI.Page
+    {
+        /// <summary>
+        /// Description for Method.</summary>
+        /// <param name="sender"> Parameter description for sender goes here</param>
+        /// <param name="e"> Parameter description for e goes here</param>
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ tags/InterpoolCloud_13_0/InterpoolCloudWebRole/FacebookCommunication/IFacebookController.cs
@@ -0,0 +1,74 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IFacebookController.cs" company="Interpool">
+//     Copyright Interpool. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace InterpoolCloudWebRole.FacebookCommunication
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using InterpoolCloudWebRole.Data;
+    using InterpoolCloudWebRole.Datatypes;
+
+    /// <summary>
+    /// Interface Description IFacebookController
+    /// </summary>
+    public interface IFacebookController
+    {
+        /// <summary>
+        /// Description for Method.</summary>
+        /// <param name="auth"> Parameter description for auth goes here</param>
+        /// <returns>
+        /// Return results are described through the returns tag.</returns>
+        string GetUserId(OAuthFacebook auth);
+
+        /// <summary>
+        /// Description for Method.</summary>
+        /// <param name="userId"> Parameter description for userId goes here</param>
+        /// <returns>
+        /// Return results are described through the returns tag.</returns>
+        List<string> GetFriendsId(string userId);
+
+        //// Returns the data of the friend with id userFriendId, who is a friend
+        //// of the user with id userId
+
+        /*
+        /// <summary>
+        /// Description for Method.</summary>
+        /// <param name="userId"> Parameter description for userId goes here</param>
+        /// <param name="userFriendId"> Parameter description for userFriendId goes here</param>
+        /// <returns>
+        /// Return results are described through the returns tag.</returns>
+        DataFacebookUser GetFriendInfo(string userId, string userFriendId);
+        */
+
+        /// <summary>
+        /// Returns all the user info by the token
+        /// </summary>
+        /// <param name="auth">Parameter description for auth goes here</param>
+        /// <returns>Return results are described through the returns tag</returns>
+        DataFacebookUser GetUserInfoByToken(OAuthFacebook auth);
+
+        //// Add a new user to the datatype userID - auth (for multiplayer)
+
+        /*
+        /// <summary>
+        /// Description for Method.</summary>
+        /// <param name="name"> Parameter description for name goes here</param>
+        /// <param name="id"> Parameter description for id goes here</param>
+        /// <param name="auth"> Parameter description for auth goes here</param>
+        void AddFriend(string name, string id, OAuthFacebook auth);
+        */
+
+        //// Save all the friends information in the db.
+
+        /// <summary>
+        /// Description for Method.</summary>
+        /// <param name="auth"> Parameter description for auth goes here</param>
+        /// <param name="game"> Parameter description for game goes here</param>
+        /// <param name="context"> Parameter description for context goes here</param>
+        void DownloadFacebookUserData(OAuthFacebook auth, Game game, InterpoolContainer context);
+    }
+}
