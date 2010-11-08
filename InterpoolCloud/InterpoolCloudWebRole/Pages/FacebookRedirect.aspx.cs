ed3f6dadb91615b423881dd23a6b2abfa96c44fd
vicenteacosta3@gmail.com <vicenteacosta3@gmail.com@ef7d9822-55ab-29b5-15d1-295a5a4f5466>
2010-11-08 20:44:47 +0000

    Pagina que simplemente redirige a Facebook

---
+++ InterpoolCloud/InterpoolCloudWebRole/Pages/FacebookRedirect.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using InterpoolCloudWebRole.FacebookCommunication;
+
+namespace InterpoolCloudWebRole.Pages
+{
+    public partial class FacebookRedirect : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            OAuthFacebook oauth = new OAuthFacebook();
+            Response.Redirect(oauth.AuthorizationLinkGet());
+        }
+    }
+}
